7528590d644892ef9feb5284f9060bb6c4a7917c
gonza7795 <gonza_7795@yahoo.com.ar>
2022-04-27 12:37:24 -0300

    Se creo componente experiencia y educacion

--- trabajo27-4/src/app/app.module.ts
+++ trabajo27-4/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
